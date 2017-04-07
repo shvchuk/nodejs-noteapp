65e9072c8e28c117f21a8692f32b3a55d2767be8
Andrzej Szewczuk <andrzej.szewczuk@gmail.com>
2017-04-07 15:32:56 +0200

    adding notes function

--- app.js
+++ app.js
@@ -2,20 +2,23 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes');
 
-let command = process.argv[2];
+const argv = yargs.argv;
+let command = argv._[0];
 console.log('Command: ', command);
+console.log('Yargs ', argv);
 
 if(command === 'add'){
-    console.log('Adding new note');
+    notes.addNote(argv.title, argv.body);
 } else if(command === 'list'){
-    console.log('Listing all notes');
+    notes.getAll();
 } else if(command === 'read'){
-    console.log('Reading the note');
+    notes.getNote(argv.title);
 } else if(command === 'remove'){
-    console.log('Removing the note');
+    notes.removeNote(argv.title);
 } else {
     console.log('Command not recognized');
 }
--- notes.js
+++ notes.js
@@ -1,6 +1,44 @@
 console.log('Starting notes.js');
 
-module.exports.addNote = () => {
-    console.log('addNote');
-    return 'New note';
+const fs = require('fs');
+
+let addNote = (title, body) => {
+    let notes = [];
+    let note = {
+        title,
+        body
+    };
+
+    try{
+        let notesString = fs.readFileSync('notes-data.json');
+        notes = JSON.parse(notesString);
+    } catch(e) {
+
+    }
+
+    let duplicateNotes = notes.filter((note) => note.title === title);
+
+    if(duplicateNotes.length === 0){
+        notes.push(note);
+        fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+    }
+};
+
+let getAll = () => {
+  console.log('Getting all notes');
+};
+
+let getNote = (title) => {
+    console.log('Reading note, title: ', title);
+};
+
+let removeNote = (title) => {
+    console.log('Removing the note, title: ', title);
+};
+
+module.exports = {
+    addNote,
+    getAll,
+    getNote,
+    removeNote
 };
