32b839d226167ad898057c1ae057ddf5b172681f
Andrzej Szewczuk <andrzej.szewczuk@gmail.com>
2017-04-09 17:37:00 +0200

    some refactoring

--- app.js
+++ app.js
@@ -12,7 +12,15 @@ console.log('Command: ', command);
 console.log('Yargs ', argv);
 
 if(command === 'add'){
-    notes.addNote(argv.title, argv.body);
+    let note = notes.addNote(argv.title, argv.body);
+    if(note){
+        console.log('New note created');
+        console.log('--');
+        console.log(`Title: ${note.title}`);
+        console.log(`Body: ${note.body}`);
+    } else {
+        console.log('Already exists the note with the title: ' + argv.title);
+    }
 } else if(command === 'list'){
     notes.getAll();
 } else if(command === 'read'){
