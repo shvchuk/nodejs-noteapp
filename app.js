5e0edea7e3315df5519a6d00c602b3a315063ae8
Andrzej Szewczuk <andrzej.szewczuk@gmail.com>
2017-04-10 15:33:25 +0200

    notes app done

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
-console.log('Starting app.js');
-
 const fs = require('fs');
 const _ = require('lodash');
 const yargs = require('yargs');
 
 const notes = require('./notes');
 
-const argv = yargs.argv;
+const titleOptions = {
+    describe: 'Title of note',
+    demand: true,
+    alias: 't'
+};
+
+const bodyOptions = {
+    describe: 'Body of the note',
+    demand: true,
+    alias: 'b'
+};
+
+const argv = yargs
+    .command('add', 'Add a new note', {
+        title: titleOptions,
+        body: bodyOptions
+    })
+    .command('list', 'List all notes')
+    .command('read', 'Read a note', {
+        title: titleOptions
+    })
+    .command('remove', 'Remove a note', {
+        title: titleOptions
+    })
+    .help()
+    .argv;
 let command = argv._[0];
-console.log('Command: ', command);
-console.log('Yargs ', argv);
 
 if(command === 'add'){
     let note = notes.addNote(argv.title, argv.body);
@@ -20,7 +41,9 @@ if(command === 'add'){
         console.log('Already exists the note with the title: ' + argv.title);
     }
 } else if(command === 'list'){
-    notes.getAll();
+    let allNotes = notes.getAll();
+    console.log(`Printing ${allNotes.length} notes.`);
+    allNotes.forEach((note) => notes.logNote(note));
 } else if(command === 'read'){
     let note = notes.getNote(argv.title);
     if(note){
--- notes.js
+++ notes.js
@@ -1,5 +1,3 @@
-console.log('Starting notes.js');
-
 const fs = require('fs');
 
 let fetchNotes = () => {
@@ -32,7 +30,7 @@ let addNote = (title, body) => {
 };
 
 let getAll = () => {
-  console.log('Getting all notes');
+  return fetchNotes();
 };
 
 let getNote = (title) => {
